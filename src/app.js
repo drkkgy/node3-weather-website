41a894155cf5847b789bf08d419f41ca22cad510
drkkgy <drkkgy@gmail.com>
2021-04-09 13:14:11 +0530

    fied bugs

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const hbs = require('hbs')
 const path = require('path')
 const express = require('express')
-const chalk = require('chalk')
+//const chalk = require('chalk')
 const geocode = require('../utils/geocode')
 const forecast = require('../utils/forecast')
 
